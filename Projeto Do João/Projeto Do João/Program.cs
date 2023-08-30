bd43bdb4449cc0837819c747cf98f39a2e4ea705
Dexyz1 <143608915+Dexyz1@users.noreply.github.com>
2023-08-30 11:52:34 -0300

    criação de menu
    utilizar if para criação
    e loop para reiniciar

---
+++ Projeto Do João/Projeto Do João/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Do_João
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            string loop = "S";
+
+            while (loop == "S")
+            {  
+                Console.WriteLine("1 - CADASTRAR novos produtos");
+                Console.WriteLine("2 - Realizar VENDA de produtos");
+                Console.WriteLine("3 - Gerar RELATORIO de produtos");
+                menu = int.Parse(Console.ReadLine());
+
+
+                if (menu == 1)
+                {
+                    Console.WriteLine("Você escolheu CADASTRAR UM PRODUTO");
+                }
+                else if (menu == 2)
+                {
+                    Console.WriteLine("Você escolheu VENDER UM PRODUTO");
+                }
+                else if (menu == 3)
+                {
+                    Console.WriteLine("Você escolheu gerar UM RELATORIO DOS PRODUTOS");
+                }
+                else
+                    Console.WriteLine("Você digitou uma opção invalida");
+
+                Console.Write("Deseja continuar no sistema? (S/N)");
+                loop = Console.ReadLine();
+
+
+               
+            }
+            Console.Write("Pressione <enter> para finalizar...");
+            Console.ReadKey();
+        }
+
+
+        }
+    }
